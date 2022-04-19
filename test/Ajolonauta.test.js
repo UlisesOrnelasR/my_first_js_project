d2f6a152d91ae8c7b20e159046e98cf2816f6cb3
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-18 21:23:53 -0600

    haciendo mi primera prueba

---
+++ test/Ajolonauta.test.js
@@ -0,0 +1,12 @@
+
+
+
+
+describe('Esto es una suite de pruebas',() => {
+    test('Caso de prueba 1', () => {
+        //Aqui tu puedes usar el codigo como lo desee utilizar
+        const result = 1 + 2
+        // Validar el resultado esperado
+        expect(result).toBe(3);
+    })
+})
